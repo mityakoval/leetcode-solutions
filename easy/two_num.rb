4e7d53362353b3250a4f00088c549e25228737a7
Mitya Koval <dima.alex.koval@gmail.com>
2017-11-03 14:53:17 -0700

    initial commit + two_num

---
+++ easy/two_num.rb
@@ -0,0 +1,26 @@
+nums = eval(ARGV[0])
+target = ARGV[1].to_i
+
+return unless nums.is_a?(Array) or target > 0
+
+class TwoNum
+  class << self
+    def do(nums, target)
+      indeces = [nil, nil]
+      nums.each.with_index do |val, index|
+        next if val > target
+        indeces[0] = index
+        leftover = target - val
+        second_index = nums.find_index(leftover)
+        if second_index and second_index != index
+          indeces[1] = second_index and break
+        else
+          next
+        end
+      end
+      puts indeces.inspect
+    end
+  end  
+end
+
+TwoNum.do(nums, target)
